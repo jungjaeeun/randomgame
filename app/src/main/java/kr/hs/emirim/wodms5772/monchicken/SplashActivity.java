7c5723139ddfa40070837d33ac4d95d2ac384dc1
Jaeeun Jung <wodms5772@naver.com>
2016-04-11 20:59:47 +0900

    #8 add plash

---
+++ app/src/main/java/kr/hs/emirim/wodms5772/monchicken/SplashActivity.java
@@ -0,0 +1,35 @@
+package kr.hs.emirim.wodms5772.monchicken;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish(); //현재 액티비티의 생을 마감
+            }
+        },3000);
+
+
+    }
+
+
+}
